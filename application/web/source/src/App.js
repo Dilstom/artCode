0608bacd14f0ebf3a70410159ccbb52d21fdacaa
Dilstom <dilyasom@gmail.com>
2023-07-19 21:50:42 -0700

    adds route with dynamic id

--- application/web/source/src/App.js
+++ application/web/source/src/App.js
@@ -1,4 +1,4 @@
-// import "./App.css";
+import "./App.css";
 import LandingPage from "./pages/landingPage/LandingPage";
 import NavbarComponent from "./components/navbar/Navbar";
 import { Container } from "react-bootstrap";
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import CoffeePage from "./pages/coffeePage/CoffeePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import CoffeePageById from "./pages/coffeePage/CoffeePageById";
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/hot" element={<CoffeePage />} />
+          <Route path="/hot/:id" element={<CoffeePageById />} />
           <Route path="/iced" element={<CoffeePage />} />
+          <Route path="/iced/:id" element={<CoffeePageById />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Container>
